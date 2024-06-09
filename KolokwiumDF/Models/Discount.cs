ec020b6acf115e0c006435479a1f0e8295011b12
Filip Spaniał <97443031+Xspils@users.noreply.github.com>
2024-06-09 18:58:20 +0200

    Update Discount.cs

--- KolokwiumDF/Models/Discount.cs
+++ KolokwiumDF/Models/Discount.cs
@@ -3,7 +3,7 @@ using namepsace KolokwiumDF.Models;
 public class Discount : DbContext
 {
     public int IdDiscount { get; set; }
-    public string Value { get; set; }
+    public int Value { get; set; }
     public DateTime DateFrom { get; set; }
     public DateTime DateTo { get; set; }
     public int IdClient { get; set; }
