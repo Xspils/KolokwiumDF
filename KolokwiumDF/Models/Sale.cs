6b3d06569c2f3c4f5b11ef7ce6eb0fdf4cb515c3
Filip Spaniał <97443031+Xspils@users.noreply.github.com>
2024-06-09 19:00:14 +0200

    Update Sale.cs

--- KolokwiumDF/Models/Sale.cs
+++ KolokwiumDF/Models/Sale.cs
@@ -5,6 +5,5 @@ public class Sale
 	public int IdSale { get; set; }
 	public int IdClient { get; set; }
 	public int IdSubscription { get; set; }
-	public string Email { get; set; }
-	public int Phone { get; set; }
+	public DateTime CreatedAt {get; set;}
 }
